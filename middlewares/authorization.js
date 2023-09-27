29a51d65511d3f8a9f743d7e5f3612f0fb2e32fc
sah_314 <khadayatesahil@gmail.com>
2023-09-27 10:03:47 +0530

    created a middleware to authorize user according to token

---
+++ middlewares/authorization.js
@@ -0,0 +1,24 @@
+const jwt = require('jsonwebtoken');
+require("dotenv").config('.env');
+
+const authorization = async(req,res,next)=>{
+    const str = req.headers['authorization'];
+    const recvtoken = str.split(" ")[1];
+    console.log(recvtoken);
+    if(recvtoken==null){
+        return res.status(404).json({"message":"Token not found"})
+    }
+    jwt.verify(recvtoken,process.env.SECRET_KEY,(err,user)=>{
+        if(err){
+            console.log(err)
+            return res.status(403).json({"message":"Token not valid"});
+        }
+        req.user = user;  
+        next();
+
+    })
+    
+}
+
+module.exports=authorization
+
