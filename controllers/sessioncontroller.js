30c46249b6a857d50a2e866a4b9ff31d95e2e24b
sah_314 <khadayatesahil@gmail.com>
2023-09-27 22:48:33 +0530

    Logic for getting user sessions complete

--- controllers/sessioncontroller.js
+++ controllers/sessioncontroller.js
@@ -1,7 +1,8 @@
-const warden = require('../models/Warden')
-require("dotenv").config('.env')
+const warden = require('../models/Warden');
+const dayToNum = require('../utils/daytonum');
+require("dotenv").config('.env');
 
-const getsessions=async(req,res)=>{
+const getSessions=async(req,res)=>{
     try {
         const sess = await warden.find({},"name sessions");
         const sessions =[];
@@ -11,7 +12,62 @@ const getsessions=async(req,res)=>{
     } catch (error) {
         res.status(501);
     }
-//res.json("getsessions");
 }
 
-module.exports = getsessions;
+const getMySessions = async(req,res)=>{
+try {
+    const uniID = req.user.userId;
+    const mysess = await warden.find({uniID:uniID},"sessions");
+    const session=[]
+    //const timenow = Date.now();
+   // console.log()
+    mysess[0].sessions.forEach((ele)=>{
+    var day =ele.day;
+    var endTime = ele.endtime;
+    var time;
+    //console.log(endTime);
+    const t1 = endTime.split(" ")[0];
+    const t2 = endTime.split(" ")[1];
+    if(t2==="AM"){
+        time = t1.slice(0,2);
+    }
+    else{
+        time = parseInt(t1.slice(0,2))+12;
+        console.log(time);
+    }
+    // console.log(t1,t2)
+    // Parse the datetime string into a JavaScript Date object
+    const sessdate = new Date();
+//console.log()
+    if(sessdate.getDay()<dayToNum(day)+1){
+        session.push(ele);
+        //console.log(sessdate.getHours());
+    }
+    else if(sessdate.getDay()==dayToNum(day)+1){
+        if(time>sessdate.getHours()){
+            session.push(ele);
+        }
+    }  
+  })
+    res.json(session);
+
+} catch (error) {
+    res.status(501).json({"message":"error"})
+}
+}
+const bookSession=async(req,res)=>{
+    const uniID = req.user.userId;
+    const {id,dayid} = req.params;
+    const updatedData = await warden.updateOne(
+        { uniID: id, "sessions._id": dayid }, 
+        {
+          $set: {
+            "sessions.$.bookedby": uniID, 
+            "sessions.$.status": "Booked",   
+          },
+        }
+      );
+    res.json(updatedData);  
+    }
+
+module.exports = ({getSessions,getMySessions,bookSession});
