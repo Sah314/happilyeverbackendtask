df8114989576e6ab161f80c542b95cb83efa62a0
sah_314 <khadayatesahil@gmail.com>
2023-09-27 22:49:13 +0530

    Added routes for getting individual and collective sessions

--- routes/sessions.js
+++ routes/sessions.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router  = express.Router();
-const getsessions = require('../controllers/sessioncontroller');
+const {getSessions,getMySessions,bookSession} = require('../controllers/sessioncontroller');
 const authorization = require('../middlewares/authorization')
 
 
-router.get('/',authorization,getsessions);
-//router.post('/',);
-
+router.get('/',authorization,getSessions);
+router.get('/Mysessions',authorization,getMySessions);
+router.put('/booksession/:id/:dayid',authorization,bookSession)
 
 module.exports=router;
