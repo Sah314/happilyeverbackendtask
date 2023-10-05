4900f18a79073738cd4be156b090a67d7b72dd6a
sah_314 <khadayatesahil@gmail.com>
2023-10-05 12:38:15 +0530

    removed sessions array:  wardenschema

--- models/Warden.js
+++ models/Warden.js
@@ -1,46 +1,64 @@
 const mongoose = require('mongoose');
 
-const sessions = new mongoose.Schema({
-    day:{
-        type:String,
-        required:true
-    },
-    starttime:{
-        type:String,
-        required:true
-    },
-    endtime:{
-        type : String,
-        required:true
-    },
-    status:{
-        type:String,
-        enum:['Pending','Booked','Completed','Unattended'],
-        default:'Pending'
-    }
-    ,
-    bookedby:{
-        type:String
-    }
-})
+// const sessions = new mongoose.Schema({
+//     day:{
+//         type:String,
+//         required:true
+//     },
+//     starttime:{
+//         type:String,
+//         required:true
+//     },
+//     endtime:{
+//         type : String,
+//         required:true
+//     },
+//     status:{
+//         type:String,
+//         enum:['Pending','Booked','Completed','Unattended'],
+//         default:'Pending'
+//     }
+//     ,
+//     bookedby:{
+//         type:String
+//     }
+// })
 
+
+
+
+// const WardenSchema = new mongoose.Schema({
+// name:{
+// type:String,
+// unique:true
+// },
+//     uniID:{
+//         type:String,
+//         unique:true,
+//         required:true
+//     },
+//     password:{
+//         type : String ,
+//         required : true
+//     },
+//     sessions:[sessions]
+// })
 
 const WardenSchema = new mongoose.Schema({
-name:{
-type:String,
-unique:true
-},
-    uniID:{
-        type:String,
-        unique:true,
-        required:true
+    uniID: {
+        type: String,
+        required: true,
+        unique: true
     },
-    password:{
-        type : String ,
-        required : true
+    password: {
+        type: String,
+        required: true
     },
-    sessions:[sessions]
-})
-
+    name: {
+        type: String,
+        required: true
+    }
+});
 const Warden = mongoose.model('Warden',WardenSchema);
+
 module.exports = Warden;
