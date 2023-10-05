43250ed4d9bac99745e5e3e48adb7ed0cbe900d1
sah_314 <khadayatesahil@gmail.com>
2023-10-05 12:38:38 +0530

    Added routes to post sessions

--- routes/sessions.js
+++ routes/sessions.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const router  = express.Router();
-const {getSessions,getMySessions,bookSession} = require('../controllers/sessioncontroller');
+const {getSessions,getMySessions,bookSession,addSession} = require('../controllers/sessioncontroller');
 const authorization = require('../middlewares/authorization')
 
-
+router.post('/addsessions',addSession)
 router.get('/',authorization,getSessions);
 router.get('/Mysessions',authorization,getMySessions);
-router.put('/booksession/:id/:dayid',authorization,bookSession)
+router.put('/booksession/:sessionId',authorization,bookSession)
 
 module.exports=router;
