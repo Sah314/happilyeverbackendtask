90ce8d1d34bab7a937855cbf80d935be24c50c45
sah_314 <khadayatesahil@gmail.com>
2023-10-05 12:39:41 +0530

    changed some things according to new schema

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -1,24 +1,24 @@
 const jwt = require('jsonwebtoken');
 require("dotenv").config('.env');
 
+
 const authorization = async(req,res,next)=>{
+
     const str = req.headers['authorization'];
     const recvtoken = str.split(" ")[1];
-    //console.log(recvtoken);
+  
     if(recvtoken==null){
         return res.status(404).json({"message":"Token not found"})
     }
-    jwt.verify(recvtoken,process.env.SECRET_KEY,(err,user)=>{
+
+    jwt.verify(recvtoken,process.env.SECRET_KEY,(err,Id)=>{
         if(err){
             console.log(err)
             return res.status(403).json({"message":"Token not valid"});
         }
-        req.user = user;  
+        req.user = Id;  
         next();
-
     })
-    
 }
-
 module.exports=authorization
 
